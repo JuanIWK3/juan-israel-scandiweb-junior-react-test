5410e7d489471ddf6f92cd2450250adebf6bec30
Juan Israel dos Anjos <juaniwk3@gmail.com>
2022-03-12 13:37:48 -0300

    first commit

---
+++ src/styles/global.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+export default createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  min-height: 100vh;
+}
+ `;
